c3676e7494afd0c7e9108a2ed20d829aa890db44
Kazuyuki Motoyama <kudakurage@gmail.com>
2019-11-21 13:51:28 +0900

    Support Font Awesome

--- code.js
+++ code.js
@@ -22,6 +22,7 @@ const defaultSymbolType = 'symbol-type-sfsymbols';
 const defaultSettingsData = { clickAction: 'create', displayType: defaultDisplayType, symbolType: defaultSymbolType, windowHeight: 600, fontSize: 40 };
 var settingsData = JSON.parse(JSON.stringify(defaultSettingsData));
 var textObjectLength = 0;
+var windowWidth = 370;
 init();
 function init() {
     figma.clientStorage.getAsync(storageKey).then(result => {
@@ -39,21 +40,21 @@ function init() {
             figma.clientStorage.setAsync(storageKey, JSON.stringify(defaultSettingsData));
             settingsData = defaultSettingsData;
         }
-        figma.ui.resize(370, parseInt(settingsData.windowHeight));
+        figma.ui.resize(windowWidth, parseInt(settingsData.windowHeight));
         figma.ui.postMessage({ settings: true, data: settingsData });
     });
 }
-function pasteFunction(nodeObjectsArray, copiedText, symbolType) {
+function pasteFunction(nodeObjectsArray, copiedText, symbolType, symbolStyle) {
     if (nodeObjectsArray.length) {
         for (let i = 0; i < nodeObjectsArray.length; i++) {
             if (nodeObjectsArray[i].type == 'TEXT') {
-                updateText(nodeObjectsArray[i], copiedText, symbolType);
+                updateText(nodeObjectsArray[i], copiedText, symbolType, symbolStyle);
                 textObjectLength++;
             }
         }
         if (textObjectLength == 0) {
             // none
-            createTextAndPaste(copiedText, symbolType);
+            createTextAndPaste(copiedText, symbolType, symbolStyle);
             textObjectLength++;
             figma.notify('Copy & Create symbol glyph object!');
         }
@@ -62,37 +63,28 @@ function pasteFunction(nodeObjectsArray, copiedText, symbolType) {
         }
     }
     else {
-        createTextAndPaste(copiedText, symbolType);
+        createTextAndPaste(copiedText, symbolType, symbolStyle);
         figma.notify('Create symbol glyph object!');
     }
     return textObjectLength;
 }
-function createFunction(copiedText, symbolType) {
+function createFunction(copiedText, symbolType, symbolStyle) {
     // console.log('createFunction')
-    createTextAndPaste(copiedText, symbolType);
+    createTextAndPaste(copiedText, symbolType, symbolStyle);
     return textObjectLength;
 }
-function updateText(selectedItem, pasteValue, symbolType) {
+function updateText(selectedItem, pasteValue, symbolType, symbolStyle) {
     return __awaiter(this, void 0, void 0, function* () {
         let selectedItemFontName = selectedItem.getRangeFontName(0, 1);
         let textStyleId = selectedItem.getRangeTextStyleId(0, 1);
         if (selectedItemFontName.family == 'SF Pro Display' || selectedItemFontName.family == 'SF Compact Display') {
             if (symbolType == "material-icons") {
-                let tempFontName = { family: '', style: '' };
-                tempFontName.family = 'Material Icons';
-                tempFontName.style = "Regular";
+                let tempFontName = fontFamilyAndStyle(symbolType, symbolStyle);
                 yield figma.loadFontAsync({ family: tempFontName.family, style: tempFontName.style });
                 selectedItem.setRangeFontName(0, selectedItem.characters.length, tempFontName);
             }
-            else {
-                yield figma.loadFontAsync({ family: selectedItemFontName.family, style: selectedItemFontName.style });
-            }
-        }
-        else if (selectedItemFontName.family == 'Material Icons') {
-            if (symbolType == "sf-symbols") {
-                let tempFontName = { family: '', style: '' };
-                tempFontName.family = 'SF Pro Display';
-                tempFontName.style = "Regular";
+            else if (symbolType == "font-awesome") {
+                let tempFontName = fontFamilyAndStyle(symbolType, symbolStyle);
                 yield figma.loadFontAsync({ family: tempFontName.family, style: tempFontName.style });
                 selectedItem.setRangeFontName(0, selectedItem.characters.length, tempFontName);
             }
@@ -101,20 +93,9 @@ function updateText(selectedItem, pasteValue, symbolType) {
             }
         }
         else {
-            if (symbolType == "sf-symbols") {
-                let tempFontName = { family: '', style: '' };
-                tempFontName.family = 'SF Pro Display';
-                tempFontName.style = "Regular";
-                yield figma.loadFontAsync({ family: tempFontName.family, style: tempFontName.style });
-                selectedItem.setRangeFontName(0, selectedItem.characters.length, tempFontName);
-            }
-            else {
-                let tempFontName = { family: '', style: '' };
-                tempFontName.family = 'Material Icons';
-                tempFontName.style = "Regular";
-                yield figma.loadFontAsync({ family: tempFontName.family, style: tempFontName.style });
-                selectedItem.setRangeFontName(0, selectedItem.characters.length, tempFontName);
-            }
+            let tempFontName = fontFamilyAndStyle(symbolType, symbolStyle);
+            yield figma.loadFontAsync({ family: tempFontName.family, style: tempFontName.style });
+            selectedItem.setRangeFontName(0, selectedItem.characters.length, tempFontName);
         }
         if (textStyleId) {
             selectedItem.setRangeTextStyleId(0, selectedItem.characters.length, textStyleId);
@@ -135,17 +116,9 @@ function updateText(selectedItem, pasteValue, symbolType) {
         selectedItem.characters = pasteValue;
     });
 }
-function createTextAndPaste(pasteValue, symbolType) {
+function createTextAndPaste(pasteValue, symbolType, symbolStyle) {
     return __awaiter(this, void 0, void 0, function* () {
-        let tempFontName = { family: '', style: '' };
-        if (symbolType == "material-icons") {
-            tempFontName.family = 'Material Icons';
-            tempFontName.style = "Regular";
-        }
-        else {
-            tempFontName.family = 'SF Pro Display';
-            tempFontName.style = "Regular";
-        }
+        let tempFontName = fontFamilyAndStyle(symbolType, symbolStyle);
         yield figma.loadFontAsync({ family: tempFontName.family, style: tempFontName.style });
         const newTextNode = figma.createText();
         newTextNode.fontName = tempFontName;
@@ -158,6 +131,32 @@ function createTextAndPaste(pasteValue, symbolType) {
         return newTextNode;
     });
 }
+function fontFamilyAndStyle(symbolType, symbolStyle) {
+    let tempFontName = { family: '', style: '' };
+    if (symbolType == "material-icons") {
+        tempFontName.family = 'Material Icons';
+        tempFontName.style = "Regular";
+    }
+    else if (symbolType == "sf-symbols") {
+        tempFontName.family = 'SF Pro Display';
+        tempFontName.style = "Regular";
+    }
+    else if (symbolType == "font-awesome") {
+        if (symbolStyle == 'brands') {
+            tempFontName.family = 'Font Awesome 5 Brands';
+            tempFontName.style = "Regular";
+        }
+        else if (symbolStyle == 'solid') {
+            tempFontName.family = 'Font Awesome 5 Free';
+            tempFontName.style = "Solid";
+        }
+        else if (symbolStyle == 'regular') {
+            tempFontName.family = 'Font Awesome 5 Free';
+            tempFontName.style = "Regular";
+        }
+    }
+    return tempFontName;
+}
 figma.ui.onmessage = message => {
     if (message.copied) {
         // console.log(settingsData.clickAction)
@@ -165,12 +164,12 @@ figma.ui.onmessage = message => {
             figma.notify('Copy symbol glyph to clipboard!');
         }
         if (settingsData.clickAction == 'paste') {
-            let num = pasteFunction(figma.currentPage.selection, message.copiedGlyph, message.symbolType);
+            let num = pasteFunction(figma.currentPage.selection, message.copiedGlyph, message.symbolType, message.symbolStyle);
             textObjectLength = 0;
         }
         if (settingsData.clickAction == 'create') {
             // console.log(settingsData.clickAction)
-            let num = createFunction(message.copiedGlyph, message.symbolType);
+            let num = createFunction(message.copiedGlyph, message.symbolType, message.symbolStyle);
             textObjectLength = 0;
             figma.notify('Create symbol glyph object!');
         }
--- code.ts
+++ code.ts
@@ -16,6 +16,7 @@ const defaultSymbolType = 'symbol-type-sfsymbols'
 const defaultSettingsData = { clickAction: 'create', displayType: defaultDisplayType, symbolType: defaultSymbolType, windowHeight: 600, fontSize: 40 }
 var settingsData = JSON.parse(JSON.stringify(defaultSettingsData));
 var textObjectLength = 0
+var windowWidth = 370
 init()
 
 function init(){
@@ -33,77 +34,59 @@ function init(){
       figma.clientStorage.setAsync(storageKey, JSON.stringify(defaultSettingsData))
       settingsData = defaultSettingsData
     }
-    figma.ui.resize(370, parseInt(settingsData.windowHeight))
+    figma.ui.resize(windowWidth, parseInt(settingsData.windowHeight))
     figma.ui.postMessage({ settings : true, data : settingsData })
   })
 }
 
-function pasteFunction(nodeObjectsArray, copiedText, symbolType){
+function pasteFunction(nodeObjectsArray, copiedText, symbolType, symbolStyle){
   if (nodeObjectsArray.length){
     for (let i = 0; i < nodeObjectsArray.length; i++) {
       if(nodeObjectsArray[i].type == 'TEXT'){
-        updateText(nodeObjectsArray[i], copiedText, symbolType)
+        updateText(nodeObjectsArray[i], copiedText, symbolType, symbolStyle)
         textObjectLength++
       }
     }
     if (textObjectLength == 0){
       // none
-      createTextAndPaste(copiedText, symbolType)
+      createTextAndPaste(copiedText, symbolType, symbolStyle)
       textObjectLength++
       figma.notify('Copy & Create symbol glyph object!')
     }else{
       figma.notify('Copy & Paste symbol glyph to selected text objects!')
     }
   }else{
-    createTextAndPaste(copiedText, symbolType)
+    createTextAndPaste(copiedText, symbolType, symbolStyle)
     figma.notify('Create symbol glyph object!')
   }
   return textObjectLength
 }
 
-function createFunction(copiedText, symbolType){
+function createFunction(copiedText, symbolType, symbolStyle){
   // console.log('createFunction')
-  createTextAndPaste(copiedText, symbolType)
+  createTextAndPaste(copiedText, symbolType, symbolStyle)
   return textObjectLength
 }
 
-async function updateText(selectedItem, pasteValue, symbolType) {
+async function updateText(selectedItem, pasteValue, symbolType, symbolStyle) {
   let selectedItemFontName = selectedItem.getRangeFontName(0, 1)
   let textStyleId = selectedItem.getRangeTextStyleId(0, 1)
   if(selectedItemFontName.family == 'SF Pro Display' || selectedItemFontName.family == 'SF Compact Display'){
     if(symbolType == "material-icons"){
-      let tempFontName = {family: '', style: ''}
-      tempFontName.family = 'Material Icons'
-      tempFontName.style = "Regular"
+      let tempFontName = fontFamilyAndStyle(symbolType, symbolStyle)
       await figma.loadFontAsync({ family: tempFontName.family, style: tempFontName.style })
       selectedItem.setRangeFontName(0, selectedItem.characters.length, tempFontName)
-    }else{
-      await figma.loadFontAsync({ family: selectedItemFontName.family, style: selectedItemFontName.style })
-    }
-  }else if(selectedItemFontName.family == 'Material Icons'){
-    if(symbolType == "sf-symbols"){
-      let tempFontName = {family: '', style: ''}
-      tempFontName.family = 'SF Pro Display'
-      tempFontName.style = "Regular"
+    }else if(symbolType == "font-awesome"){
+      let tempFontName = fontFamilyAndStyle(symbolType, symbolStyle)
       await figma.loadFontAsync({ family: tempFontName.family, style: tempFontName.style })
       selectedItem.setRangeFontName(0, selectedItem.characters.length, tempFontName)
     }else{
       await figma.loadFontAsync({ family: selectedItemFontName.family, style: selectedItemFontName.style })
     }
   }else{
-    if(symbolType == "sf-symbols"){
-      let tempFontName = {family: '', style: ''}
-      tempFontName.family = 'SF Pro Display'
-      tempFontName.style = "Regular"
-      await figma.loadFontAsync({ family: tempFontName.family, style: tempFontName.style })
-      selectedItem.setRangeFontName(0, selectedItem.characters.length, tempFontName)
-    }else{
-      let tempFontName = {family: '', style: ''}
-      tempFontName.family = 'Material Icons'
-      tempFontName.style = "Regular"
-      await figma.loadFontAsync({ family: tempFontName.family, style: tempFontName.style })
-      selectedItem.setRangeFontName(0, selectedItem.characters.length, tempFontName)
-    }
+    let tempFontName = fontFamilyAndStyle(symbolType, symbolStyle)
+    await figma.loadFontAsync({ family: tempFontName.family, style: tempFontName.style })
+    selectedItem.setRangeFontName(0, selectedItem.characters.length, tempFontName)
   }
 
   if(textStyleId){
@@ -124,15 +107,8 @@ async function updateText(selectedItem, pasteValue, symbolType) {
   selectedItem.characters = pasteValue
 }
 
-async function createTextAndPaste(pasteValue, symbolType) {
-  let tempFontName = {family: '', style: ''}
-  if(symbolType == "material-icons"){
-    tempFontName.family = 'Material Icons'
-    tempFontName.style = "Regular"
-  }else{
-    tempFontName.family = 'SF Pro Display'
-    tempFontName.style = "Regular"
-  }
+async function createTextAndPaste(pasteValue, symbolType, symbolStyle) {
+  let tempFontName = fontFamilyAndStyle(symbolType, symbolStyle)
   await figma.loadFontAsync({ family: tempFontName.family, style: tempFontName.style })
   const newTextNode = figma.createText()
   newTextNode.fontName = tempFontName
@@ -146,6 +122,29 @@ async function createTextAndPaste(pasteValue, symbolType) {
   return newTextNode;
 }
 
+function fontFamilyAndStyle(symbolType, symbolStyle){
+  let tempFontName = {family: '', style: ''}
+  if(symbolType == "material-icons"){
+    tempFontName.family = 'Material Icons'
+    tempFontName.style = "Regular"
+  }else if(symbolType == "sf-symbols"){
+    tempFontName.family = 'SF Pro Display'
+    tempFontName.style = "Regular"
+  }else if(symbolType == "font-awesome"){
+    if(symbolStyle == 'brands'){
+      tempFontName.family = 'Font Awesome 5 Brands'
+      tempFontName.style = "Regular"
+    }else if(symbolStyle == 'solid'){
+      tempFontName.family = 'Font Awesome 5 Free'
+      tempFontName.style = "Solid"
+    }else if(symbolStyle == 'regular'){
+      tempFontName.family = 'Font Awesome 5 Free'
+      tempFontName.style = "Regular"
+    }
+  }
+  return tempFontName
+}
+
 figma.ui.onmessage = message => {
   if (message.copied) {
     // console.log(settingsData.clickAction)
@@ -153,12 +152,12 @@ figma.ui.onmessage = message => {
       figma.notify('Copy symbol glyph to clipboard!')
     }
     if (settingsData.clickAction == 'paste'){
-      let num = pasteFunction(figma.currentPage.selection, message.copiedGlyph, message.symbolType)
+      let num = pasteFunction(figma.currentPage.selection, message.copiedGlyph, message.symbolType, message.symbolStyle)
       textObjectLength = 0
     }
     if (settingsData.clickAction == 'create'){
       // console.log(settingsData.clickAction)
-      let num = createFunction(message.copiedGlyph, message.symbolType)
+      let num = createFunction(message.copiedGlyph, message.symbolType, message.symbolStyle)
       textObjectLength = 0
       figma.notify('Create symbol glyph object!')
     }
